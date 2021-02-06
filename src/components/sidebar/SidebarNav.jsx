d8e09094ea218dffb3a5087b3543ba4b2679f204
NikolaGrujicic <nikola.grujicic.03@gmail.com>
2021-02-06 14:09:12 +0100

    added sidebar dividers

--- src/components/sidebar/SidebarNav.jsx
+++ src/components/sidebar/SidebarNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Divider } from 'semantic-ui-react';
 import SideBarItem from './sideBarItem/SideBarItem';
 import '../../styles/scss/SideBar.scss';
 
@@ -8,13 +8,17 @@ const SideBarNav = () => (
     <SideBarItem highlight label="Home" icon="home" />
     <SideBarItem label="Trending" icon="fire" />
     <SideBarItem label="Followers" icon="spy" />
+    <Divider />
     <SideBarItem label="History" icon="history" />
     <SideBarItem label="Watch later" icon="clock" />
     <SideBarItem label="Liked videos" icon="thumbs up" />
+    <Divider />
     <SideBarItem label="Movies and Shows" icon="film" />
+    <Divider />
     <SideBarItem label="Report history" icon="flag" />
     <SideBarItem label="Help" icon="help circle" />
     <SideBarItem label="Send feedback" icon="comment" />
+    <Divider />
   </Menu>
 );
 
